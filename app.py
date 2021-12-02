ab61fe363e1b1185c15a9560311a48e2d780cb58
hbyr99 <henry.louis.980@gmail.com>
2021-12-02 15:11:17 -0500

    Base grid

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html',
-                           subtitle='Home Page',
-                           text='This is the home page')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

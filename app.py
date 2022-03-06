f519f34cd28e0548b16bed33fe7fb2ebb22b3b98
Henry Baldacci <henry.louis.980@gmail.com>
2022-03-06 15:57:48 -0500

    Progress on jQuery

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
-
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    with open('elements.json', 'r') as src:
+        elements = json.loads(src.read())
+
+    return render_template('home.html', elements = elements)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,14 +1,33 @@
 /* Script to select a single table value
  * Magic unicorn crap
  */
+
+console.log(elements);
+
+function getElementByAtomicNumber(atomicNumber) {
+    return elements.find( ({ number }) => number === atomicNumber ); 
+}
+
 $(document).ready(function(){
-    $('.element').click( function() {
+    $('.element.ptable').click( function() {
+        var elementID = $(this).text().replace(/\D/g,'');
+        var selectedElement = getElementByAtomicNumber(parseInt(elementID));
+
         if ( $(this).hasClass('clicked')) {
             $(this).removeClass('clicked');
-           // $(this).css('background-color', '');
+            $("#" + elementID).remove();
+
         } else {
+            var classList = $(this).attr('class').split(/\s+/);
+            console.log(classList);
             $(this).addClass('clicked');
-            // $(this).css('background-color', 'white');
+            $("<div></div>").attr('id', elementID).appendTo('.detailedView');
+            $("#" + elementID).addClass(['element', classList[2]]);
+            $("#" + elementID).append('<span>' + selectedElement.number + '<br />' 
+            + selectedElement.name + '<br />' 
+            + selectedElement.symbol + '<br />' 
+            + selectedElement.aMass + '<br />' 
+            + '<input type="text" class="textInput" value="' + selectedElement.eConfig + '">' + '</span>');
         }
        
     });

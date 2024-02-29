d28dc4595dc4204afc6796a27a72595a95e14e9c
VictorNorman <vtn2@calvin.edu>
2024-02-29 13:59:52 -0500

    make subtabs work

--- src/scripts/tabHandling.ts
+++ src/scripts/tabHandling.ts
@@ -1,22 +1,31 @@
 export function handleTabSwitching() {
   const tabs = document.querySelectorAll('.tab');
-  const tabContents = document.querySelectorAll('.tab-panel');
 
   tabs.forEach(tab => {
     tab.addEventListener('click', () => {
+      const parent = tab.parentElement;
+      const siblingTabs = parent!.children;
 
-      const target = document.querySelector((tab as any).dataset.target);
+      // get all tabPanels for siblingTabs.
+      const siblingTabPanels = [];
+      for (let s of siblingTabs) {
+        siblingTabPanels.push(document.querySelector((s as any).dataset.target));
+      }
+      // clear all is-active classes on sibling tab-panels.
+      siblingTabPanels.forEach(stp => stp.classList.remove('is-active'));
 
-      // clear all is-active classes on tab-panels.
-      tabContents.forEach(tc => tc.classList.remove('is-active'));
       // make the one selected tab-panel active.
+      // get the target panel that corresponds to the clicked tab.
+      const target = document.querySelector((tab as any).dataset.target);
       target.classList.add('is-active');
 
       // clear the coloring on the tabs
-      tabs.forEach(t => t.classList.remove('is-active'));
+      // tabs.forEach(t => t.classList.remove('is-active'));
+      for (let s of siblingTabs) {
+        s.classList.remove('is-active');
+      }
       // make the selected tab colored
       tab.classList.add('is-active');
-
     });
   });
 }
